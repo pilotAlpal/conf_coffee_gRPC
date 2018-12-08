590b33d9531d965dd9169bf480b78904ff29899c
pilotAlpal <lsegar01@ucm.es>
2018-12-08 22:05:10 +0100

    install.sh created

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-8-jdk openjdk-8-jdk-headless maven git
+git clone https://github.com/escalope/coffeegrpc.git
+cd coffeegrpc
+mvn clean package
+mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args="50051"
