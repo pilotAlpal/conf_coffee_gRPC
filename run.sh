eb0a149fe61974756992955309b6c783d7594875
sergio semedi <ssemedi@opennebula.systems>
2018-12-09 13:45:14 +0100

    splitted scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -rf coffeegrpc
-git clone https://github.com/softportal/coffeegrpc.git
 cd coffeegrpc
 mvn clean package
 mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args="50051"
