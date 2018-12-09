f32bac6648bafaad80511ae53825204d8487d2b9
Lucas <lsegar01@ucm.es>
2018-12-09 13:21:50 +0100

    hotfix

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pkill -9 -f coffeegrpc
 rm -rf coffeegrpc
-git clone git@github.com:softportal/coffeegrpc.git
+git clone https://github.com/softportal/coffeegrpc.git
 cd coffeegrpc
 mvn clean package
 mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args="50051"
