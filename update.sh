050c72b5603f142b3c7bdf0e204163354953511c
pilotAlpal <lsegar01@ucm.es>
2018-12-09 12:27:58 +0100

    created update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pkill -9 -f coffeegprc
+rm -rf coffeegprc
+git clone git@github.com:softportal/coffeegrpc.git
+cd coffeegrpc
+mvn clean package
+mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args="50051"
