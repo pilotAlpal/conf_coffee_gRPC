e39bc46bac3ebeebfb33634fff8cb2b821899b31
sergio semedi <ssemedi@opennebula.systems>
2018-12-10 12:02:17 +0100

    avoiding stdout

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y openjdk-8-jdk openjdk-8-jdk-headless maven git
+sudo apt update > /dev/null
+sudo apt install -y openjdk-8-jdk openjdk-8-jdk-headless maven git > /dev/null
 git clone https://github.com/escalope/coffeegrpc.git
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd coffeegrpc
-mvn clean package
-mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args="50051"
+mvn clean package > /dev/null
+cmd="mvn clean compile exec:java -Dexec.mainClass=iotucm.coffeeservice.CoffeeServiceServer -Dexec.args='50051'"
+nohup $cmd &
